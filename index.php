d7285084863edee1ca0cb1d275f5e0ad58938c6f
alexanderRDProjects <a.rdunn.yw@gmail.com>
2016-06-23 10:38:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,4 @@
-<?php echo phpinfo() ?>
+clients screen resolution: <script type='text/javascript'>document.write(screen.width+'x'+screen.height); </script>
+referer: <?php print ($_SERVER['HTTP_REFERER']); ?>
+client ip: <?php print ($_SERVER['REMOTE_ADDR']); ?>
+user agent:  <?php print ($_SERVER['HTTP_USER_AGENT']); ?>
