85d58a9f82bc7a8c1cc59b66756f59cf9cf92883
alexanderRDProjects <a.rdunn.yw@gmail.com>
2016-06-23 11:24:10 +0100

    added head

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
+<html>
+<head>
+  <title>PHP Tracer</title>
+  <link rel='shortcut icon' href='favicon.ico' type='image/x-icon'/ > 
+</head>
 <p>clients screen resolution: <script type='text/javascript'>document.write(screen.width+'x'+screen.height); </script></p>
 <p>referer: <?php print ($_SERVER['HTTP_REFERER']); ?></p>
 <p>client ip: <?php print ($_SERVER['REMOTE_ADDR']); ?></p>
 <p>user agent:  <?php print ($_SERVER['HTTP_USER_AGENT']); ?></p>
+</html>
