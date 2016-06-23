7e49c67466ff10083b2aea87eff2d07640608740
alexanderRDProjects <a.rdunn.yw@gmail.com>
2016-06-23 11:18:06 +0100

    added whitespace

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-clients screen resolution: <script type='text/javascript'>document.write(screen.width+'x'+screen.height); </script>
-referer: <?php print ($_SERVER['HTTP_REFERER']); ?>
-client ip: <?php print ($_SERVER['REMOTE_ADDR']); ?>
-user agent:  <?php print ($_SERVER['HTTP_USER_AGENT']); ?>
+<p>clients screen resolution: <script type='text/javascript'>document.write(screen.width+'x'+screen.height); </script></p>
+<p>referer: <?php print ($_SERVER['HTTP_REFERER']); ?></p>
+<p>client ip: <?php print ($_SERVER['REMOTE_ADDR']); ?></p>
+<p>user agent:  <?php print ($_SERVER['HTTP_USER_AGENT']); ?></p>
